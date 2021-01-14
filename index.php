26efbbf1b93d467986883b335d8a7be22936af31
Trzmielu <eryk.podborny@gmail.com>
2021-01-14 12:59:34 +0100

    azure

--- index.php
+++ index.php
@@ -208,33 +208,6 @@
                                 ?>
                             </select>
                     </div>
-                    <?php
-                                    // Initialize connection variables.
-                                    $host = "resourcemanagerdb.postgres.database.azure.com";
-                                    $database = "baza_aplikacja";
-                                    $user = "resourcemanager@resourcemanagerdb";
-                                    $password = "Trzmielu123";                                    
-                                    // Initialize connection object.
-                                    $connection = pg_connect("host=$host dbname=$database user=$user password=$password")
-                                                or die("Failed to create connection to database: ". pg_last_error(). "<br/>");
-
-                                    print "Successfully created connection to database. <br/>";
-
-                                    // Perform some SQL queries over the connection.
-                                    $query = "SELECT * from sprzet";
-                                    $result_set = pg_query($connection, $query) 
-                                        or die("Encountered an error when executing given sql statement: ". pg_last_error(). "<br/>");
-                                    while ($row = pg_fetch_row($result_set))
-                                    {
-                                        print "Data row = ($row[0], $row[1], $row[2]). <br/>";
-                                    }
-
-                                    // Free result_set
-                                    pg_free_result($result_set);
-
-                                    // Closing connection
-                                    pg_close($connection);
-                                ?>
                     <!-- Content Row -->
                     <div id="row1" class="row">
 
