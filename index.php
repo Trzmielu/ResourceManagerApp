ae8b3ec712ec816c0627325364059d71a7ea766e
Trzmielu <eryk.podborny@gmail.com>
2021-01-14 12:58:02 +0100

    azure

--- index.php
+++ index.php
@@ -222,7 +222,12 @@
 
                                     // Perform some SQL queries over the connection.
                                     $query = "SELECT * from sprzet";
-                                    pg_query($connection, $query) 
+                                    $result_set = pg_query($connection, $query) 
+                                        or die("Encountered an error when executing given sql statement: ". pg_last_error(). "<br/>");
+                                    while ($row = pg_fetch_row($result_set))
+                                    {
+                                        print "Data row = ($row[0], $row[1], $row[2]). <br/>";
+                                    }
 
                                     // Free result_set
                                     pg_free_result($result_set);
