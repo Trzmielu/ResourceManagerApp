e72be4c35c8554523ba0da057f07e20008a552e0
Trzmielu <eryk.podborny@gmail.com>
2021-01-20 13:48:32 +0100

    azure

--- usun_status_licencja.php
+++ usun_status_licencja.php
@@ -163,6 +163,8 @@
 
             <!-- Main Content -->
             <div id="content">                  
+                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                </nav>
 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
@@ -171,8 +173,7 @@
                     <div class="text-center">
                         <div class="error mx-auto" data-text="404">404</div>
                         <p class="lead text-gray-800 mb-5">Page Not Found</p>
-                        <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
-                        <a href="index.html">&larr; Back to Dashboard</a>
+                        <a href="index.php">&larr; Powrót do Dashboard'u</a>
                     </div>
 
                 </div>
